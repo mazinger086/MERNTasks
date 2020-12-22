62c73bc92359f72d23538a43ad6a8a68afa638b3
mazinger086 <mazinger_086@yahoo.com>
2020-12-22 10:49:01 -0300

    Creando el repositorio de MERN Tasks

---
+++ src/components/auth/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return ( 
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark"></div>
+        </div>
+     );
+}
+ 
+export default Login;
